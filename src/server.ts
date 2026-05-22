4fd6123b728ae134e603c923a4ab9f5b929845e5
TanjimulSabbir <tanjimulsabbir.dev@gmail.com>
2026-05-22 21:52:13 +0600

    feat: add TypeScript configuration and update dependencies

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,6 @@
 import app from "./app"
 import { config } from "./config"
 
-
 const main = () => {
     app.listen(config.port, () => {
         console.log(`Server is running on port ${config.port}`);
